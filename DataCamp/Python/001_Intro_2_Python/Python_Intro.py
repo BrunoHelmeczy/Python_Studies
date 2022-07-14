a3eef7848a0ff886fee87903731d687c5401868c
Bruno Helmeczy <helmeczybruno32@gmail.com>
2022-07-14 22:32:54 +0200

    🔥 Add ch2 to intro_2_Python on datacamp

--- DataCamp/Python/001_Intro_2_Python/Python_Intro.py
+++ DataCamp/Python/001_Intro_2_Python/Python_Intro.py
@@ -48,7 +48,60 @@ pi_str = "3.1415926"
 float(pi_str)
 
 # 2) Lists  ----
+heights = [1.80, 1.66, 1.79, 1.83, 1.60, 1.62]
+namesheights = ['dad', 1.80,'mom',  1.66, 'ubi',  1.79, 'buda', 1.83, 'frutti',  1.60, 'pofi',  1.62]
+namesheights2 = [['dad', 1.80], ['mom', 1.66], ['ubi', 1.79], ['buda', 1.83], ['frutti', 1.60], ['pofi', 1.62]]
 
+type(heights)
+type(namesheights)
+type(namesheights2)
 
+# Ex 1-3 ---
+a = 31
+b = 43
+c = 95
+d = 25
+e = 2394
+mylist = [a, b, c, d, e]
+[1+2, 'a' * 5, 3]
+
+    # Subsetting
+heights[0]   # --> 1st element
+heights[0:3] # --> 1st to 3rd element
+heights[:4]  # --> up to 4th element
+heights[1:]  # --> starting from 2nd element
+heights[-1]  # --> get last element 
+heights[-2]  # --> get 2nd to last element 
+
+namesheights2[2][1]
+namesheights2[2:4][1]
+
+    # List manipulation
+namesheights[7] = 1.81
+namesheights[8:10] = ['fruzsina', 1.59]
+namesheightsall = namesheights + ['mark', 1.2, 'lena', 1.65]
+del(namesheightsall[12:])
+namesheightsall
+
+x = ['a', 'b', 'c']
+y = x
+    # values are updated by reference --> i.e. a lá data.table 
+    # --> copy() method / [:] slicing to list all elements explicitly / wrap in list()
+x[2] = 'bla'
+z = y.copy()
+z[2] = 'c'
+
+a = x[:]
+a[2] = 'c'
+
+x
+y
+z
+a
+
+# +1: ; sign 4 1+ commands on 1 line:
+mylist = list(range(1,10)) ; print(mylist[:5])
+
+mylist[-4:-2]
 # 3) Functions & Packages ----
 # 4) NumPy ----
