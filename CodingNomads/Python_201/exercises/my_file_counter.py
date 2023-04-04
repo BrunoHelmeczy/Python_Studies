669d1990b3ed39232e0f49279f3886a4ae8c75ce
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-04 13:54:03 +0200

    add writing to filecounter

--- CodingNomads/Python_201/exercises/my_file_counter.py
+++ CodingNomads/Python_201/exercises/my_file_counter.py
@@ -1,6 +1,7 @@
 # count all file types under your desktop 
-
 import pathlib
+import os
+from datetime import datetime
 from collections import Counter
 
 def countFilesUnderDesktop():
@@ -12,3 +13,20 @@ def countFilesUnderDesktop():
 
     [print(f"I found {v} files with extension {k} under your Desktop") for k, v in filesout.items()]
     return filesout
+
+def writeFileCount2File():
+    home = pathlib.Path.home()
+    # cwd = pathlib.Path.cwd()
+    DataFolder = home / 'Desktop' / 'My_Professional_Development' / 'Python_Studies' / 'CodingNomads' / 'Python_201' / 'data'
+    DataFolder.mkdir(exist_ok = True)
+    os.chdir(DataFolder)
+
+    myfile = open('check.txt', 'a')
+    filecount = countFilesUnderDesktop()
+
+    TimeNow = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    myfile.write(f"\n{TimeNow}: {str(filecount)}")
+    myfile.close()
+
+writeFileCount2File()
