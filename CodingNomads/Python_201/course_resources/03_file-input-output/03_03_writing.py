3e101c8c179519c53ce7530039dc76f168d2ed19
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-04 16:45:36 +0200

    add input ex3

--- CodingNomads/Python_201/course_resources/03_file-input-output/03_03_writing.py
+++ CodingNomads/Python_201/course_resources/03_file-input-output/03_03_writing.py
@@ -1,2 +1,14 @@
 # Write a script that reads in the contents of `words.txt`
 # and writes the contents in reverse to a new file `words_reverse.txt`.
+import pathlib
+
+txt = pathlib.Path().resolve().joinpath('CodingNomads/Python_201/course_resources/03_file-input-output')
+
+with txt.joinpath('words.txt').open('r') as Input:
+    text = Input.read()
+    words = text.split('\n')
+    out = '\n'.join([w for w in reversed(words)])
+
+with txt.joinpath('words_reverse.txt').open('a') as Output:
+    Output.write(out)
+
