fb4a742f4fb281dee27cca4d1b09a370d728f13d
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-04 16:27:23 +0200

    add input ex1

--- CodingNomads/Python_201/course_resources/03_file-input-output/03_01_long_words.py
+++ CodingNomads/Python_201/course_resources/03_file-input-output/03_01_long_words.py
@@ -1,2 +1,10 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
+import pathlib
+
+txt = pathlib.Path().resolve().joinpath('CodingNomads/Python_201/course_resources/03_file-input-output/words.txt')
+
+with txt.open('r') as Input:
+    text = Input.read()
+    words = text.split('\n')
+    hide = [print(w) for w in words if len(w) > 20]
