51b135539692b3d7cdc09ae078550c26258dcfff
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-04 14:54:15 +0200

    add file read counter f(x)

--- CodingNomads/Python_201/exercises/my_file_counter.py
+++ CodingNomads/Python_201/exercises/my_file_counter.py
@@ -1,6 +1,7 @@
 # count all file types under your desktop 
 import pathlib
 import os
+import re
 from datetime import datetime
 from collections import Counter
 
@@ -14,14 +15,15 @@ def countFilesUnderDesktop():
     [print(f"I found {v} files with extension {k} under your Desktop") for k, v in filesout.items()]
     return filesout
 
-def writeFileCount2File():
+def writeFileCount2File(filename = 'check.txt'):
     home = pathlib.Path.home()
     DataFolder = home / 'Desktop' / 'My_Professional_Development' / 'Python_Studies' / 'CodingNomads' / 'Python_201' / 'data'
     DataFolder.mkdir(exist_ok = True)
     cwd = os.getcwd()
     os.chdir(DataFolder)
 
-    myfile = open('check.txt', 'a')
+    myfile = open(filename, 'a')
+
     filecount = countFilesUnderDesktop()
 
     TimeNow = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -31,4 +33,24 @@ def writeFileCount2File():
 
     os.chdir(cwd)
 
+def readFileCountFile(filename = 'check.txt'):
+    home = pathlib.Path.home()
+    DataFolder = home / 'Desktop' / 'My_Professional_Development' / 'Python_Studies' / 'CodingNomads' / 'Python_201' / 'data'
+    DataFolder.mkdir(exist_ok = True)
+    cwd = os.getcwd()
+    os.chdir(DataFolder)
+
+    myfile = open(filename, 'r')
+    content = myfile.read()
+    contlist = content.split('\n')
+
+    splits = [s.strip() for s in re.sub('^.+ {(.+)}$', '\\1', contlist[-1]).split(',')]
+    origdict = {x.split(':')[0].replace("'",''): int(x.split(':')[1]) for x in splits}
+
+    myfile.close()
+    os.chdir(cwd)
+
+    return sum(origdict.values())
+
 writeFileCount2File()
+readFileCountFile()
