740df9904be8fca37980927ea5422ef93de5ec42
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-05 10:48:51 +0200

    add py2 f(x) ex1

--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_01_fix_function_definition.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_01_fix_function_definition.py
@@ -3,8 +3,49 @@
 # After your fixes, the function should allow you to take a name as an input
 # and return a greeting message that you can save to a variable.
 
-function say_hello("name"):
-        return print(f"Hello {name}!")
+def say_hello(name):
+	return print(f"Hello {name}!")
 
 greeting = hello(name)
 print(greeting)
+
+# args = ['bla', 'blabla']
+def greet_many(greeting, *args):
+	# """_summary_
+
+	# Args:
+	# 	greeting (_type_): _description_
+
+	# Returns:
+	# 	_type_: _description_
+	# """    
+    
+    greetings = '\n'.join([f"{greeting}, {name}! How are you?" for name in args])
+    return greetings
+
+print(greet_many('Hello','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'))
+
+# kwargs = {'george': 'IT', 'kate': 'architecture'}
+def greet_many2(greeting: str = 'Hello', **kwargs):
+	"""
+	Generates a greeting.
+
+	Args:
+		greeting (str): blabla
+		kwargs: blablablablabla
+
+	Returns:
+		str: personalized greeting messages
+	"""
+
+	greetings = '\n'.join([f"{greeting} {k} from {v}!!!" for k, v in kwargs.items()])
+	return greetings
+
+help(greet_many2)
+
+print(greet_many2( George = 'IT', Kate = 'Architecture', Bruno = 'Data'))
+print(greet_many2('Hello', George = 'IT', Kate = 'Architecture', Bruno = 'Data'))
+
+my_list = ["apple", "banana", "orange"]
+obj1 = enumerate(my_list)
+print(obj1)
