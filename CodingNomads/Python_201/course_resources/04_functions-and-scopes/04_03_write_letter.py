c196ce35cd92393d82f7b6646241deea1f950e89
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-05 10:53:17 +0200

    add f(x) ex3

--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_03_write_letter.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_03_write_letter.py
@@ -3,3 +3,10 @@
 # message with the `name` input, as well as a goodbye message that uses
 # the `name` again. Combine that with the input `text` to return a
 # complete `letter`.
+
+def write_letter(name: str, text: str = 'Lovely to meet you'):
+    greeting = f"Hello {name}, {text} \n"
+    goodbuy  = f"Good Buy {name}, it was {text}"
+    return greeting + goodbuy
+
+print(write_letter('Bruno'))
--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_04_combine_functions.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,25 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+def greet_many(greeting: str = 'Hello', **kwargs):
+	"""
+	Generates a greeting.
+
+	Args:
+		greeting (str): blabla
+		kwargs: blablablablabla
+
+	Returns:
+		str: personalized greeting messages
+	"""
+
+	greetings = '\n'.join([f"{greeting} {k} from {v}!!!" for k, v in kwargs.items()])
+	return greetings
+
+def write_letter(name: str, text: str = 'Lovely to meet you'):
+    greeting = f"Hello {name}, {text} \n"
+    goodbuy  = f"Good Buy {name}, it was {text}"
+    return greeting + goodbuy
+
+
