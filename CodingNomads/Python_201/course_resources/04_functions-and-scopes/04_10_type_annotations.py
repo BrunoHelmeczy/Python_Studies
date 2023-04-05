d19d2aa58e57420fc6559d16ea5c515caad6c75d
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-05 11:34:15 +0200

    add type hints ex10

--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_10_type_annotations.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_10_type_annotations.py
@@ -1,12 +1,12 @@
 # Add type annotations to the three functions shown below.
 
-def multiply(num1, num2):
+def multiply(num1: float, num2: float):
     return num1 * num2
 
-def greet(greeting, name):
+def greet(greeting: str, name: str):
     sentence = f"{greeting}, {name}! How are you?"
     return sentence
 
-def shopping_list(*args):
+def shopping_list(*args: any):
     [print(f"* {item}") for item in args]
     return args
