4f0f58d5f623c8cb714843c4f14938fe8f4404fc
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-05 11:26:00 +0200

    add divcheck ex7

--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_07_determine_divisible.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_07_determine_divisible.py
@@ -1,9 +1,17 @@
 # Write a script where you complete the following tasks:
-# - define a function that determines whether the number is
-#   divisible by 4 OR 7 and returns a boolean
-# - define a function that determines whether a number is
-#   divisible by both 4 AND 7 and returns a boolean
 # - take in a number from the user between 1 and 1,000,000,000
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+
+def divisibleBy4and7(nr: int = 1000000):
+    return nr % 4 == 0 and nr % 7 == 0
+
+def divisibleBy4or7(nr: int = 1000000):
+    return nr % 4 == 0 or nr % 7 == 0
+
+def checkdiv(Nr: int):
+    print(f"Is {Nr} divisible either by 4 or 7 ? Answer: {divisibleBy4or7(Nr)}")
+    print(f"Is {Nr} divisible either by 4 and 7 ? Answer: {divisibleBy4and7(Nr)}")
+
+checkdiv(1023488)
