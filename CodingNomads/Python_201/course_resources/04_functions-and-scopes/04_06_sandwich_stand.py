33f81bfcaa0187557b35055e88ebce821edc2f12
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-05 11:02:42 +0200

    add args ex6

--- CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_06_sandwich_stand.py
+++ CodingNomads/Python_201/course_resources/04_functions-and-scopes/04_06_sandwich_stand.py
@@ -3,3 +3,13 @@
 # - takes an arbitrary amount of toppings
 # - returns a string representing a sandwich with the bread on top
 #   and bottom, and the toppings in between.
+
+def make_sandwich(bread = 'white', *args):
+    out = [item for item in args]
+
+    out.insert(0, bread + ' bread')
+    out.append(bread + ' bread')
+
+    return ' + '.join(out)
+
+make_sandwich('multi-grain', 'ham', 'cheese', 'lettuce', 'tomato')
