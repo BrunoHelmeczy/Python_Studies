2b9064af7cbad84d4ca120553e5fcb60cd8b94d8
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-06 14:50:57 +0200

    add ch6 exs6-8 - python 201 - codingnomads

--- CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_06_shirt_shop.py
+++ CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_06_shirt_shop.py
@@ -1,5 +1,8 @@
 # Using a list comprehension, create a *cartesian product* (google this!)
 # of the given lists. Then open up your online shop ;)
+from itertools import product
 
 colors = ["neon orange", "spring green"]
 sizes = ["S", "M", "L"]
+
+['-'.join(x) for x in product(colors, sizes)]
