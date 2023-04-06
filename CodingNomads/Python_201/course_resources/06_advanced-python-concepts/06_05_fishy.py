7bb88a43c11ea8deb8e430bacf432921f42e2899
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-06 14:30:25 +0200

    add ch6 ex5 - python201 - codingnomads

--- CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_05_fishy.py
+++ CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_05_fishy.py
@@ -1,4 +1,5 @@
 # Using a listcomp, create a list from the following tuple that includes
 # only words ending with -fish. Tip: Use an `if` statement in the listcomp.
-
 fish_tuple = ('blowfish', 'clownfish', 'catfish', 'octopus')
+
+[fish for fish in fish_tuple if fish.endswith('fish')]
