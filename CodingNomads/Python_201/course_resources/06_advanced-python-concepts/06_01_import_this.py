19c4ab652abcb8e427bc053523448e8090a6b391
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-06 14:18:46 +0200

    add ch6 ex1

--- CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_01_import_this.py
+++ CodingNomads/Python_201/course_resources/06_advanced-python-concepts/06_01_import_this.py
@@ -3,7 +3,17 @@
 # All the necessary variables and functions are
 # already defined in the `codingnomads/` folder.
 
+import pathlib
+import os
 
+origcwd = pathlib.Path().resolve()
+codingnomadspath = list(origcwd.rglob('*201/*/06_*/'))[0]
+
+os.chdir(path = codingnomadspath)
+from codingnomads.recipes.soup import make_soup
+from codingnomads.ingredients import potato
+
+os.chdir(origcwd)
 
 soup = make_soup(potato)
 print(soup)
