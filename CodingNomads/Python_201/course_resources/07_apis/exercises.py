9a9e7b13c0c8cb22617c22a93ea30e1d51e3698c
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-04-11 10:49:10 +0200

    add api exercises

---
+++ CodingNomads/Python_201/course_resources/07_apis/exercises.py
@@ -0,0 +1,57 @@
+import requests
+
+# 1) Get users data & print out status / encoding / content
+def getUsers():
+    users = "http://demo.codingnomads.co:8080/tasks_api/users"
+    res = requests.get(users)
+    return res.json()['data']
+
+
+users = "http://demo.codingnomads.co:8080/tasks_api/users"
+res = requests.get(users)
+
+res.status_code
+res.encoding
+res.content
+
+
+# 2) make list of users emails
+data = getUsers()
+
+emails = [d['email'] for d in data]
+
+# 3) add yourself as user with POST request --> validate by refreshing get() in 01)
+body = {
+    # "id": 592,
+    "first_name": "Brúnó", 
+    "last_name": "Helmeczy", 
+    "email": "helmeczybruno32@gmail.com" 
+}
+
+def addUser(body_dict):
+    users = "http://demo.codingnomads.co:8080/tasks_api/users"
+    res = requests.post(users, json = body_dict)
+
+    print(res.status_code)
+
+addUser(body)
+
+# getUsers()
+
+# 4) PUT request to update your info --> validate by refreshing 01)
+def updateUser(body_dict = body):
+    users = "http://demo.codingnomads.co:8080/tasks_api/users/592"
+    res = requests.put(users, json = body_dict)
+
+    print(res.status_code)
+    
+updateUser(body)
+
+# 5) DELETE req to remove yourself --> validate
+def deleteUser():
+    users = "http://demo.codingnomads.co:8080/tasks_api/users/593"
+    res = requests.delete(users)
+
+    print(res.status_code)
+
+
