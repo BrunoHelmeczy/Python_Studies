6c52fa6b8d8ee4750f90765cd1a0707ed99ae952
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-06-23 10:08:10 +0200

    add eda plot1 + lm with categ vars

--- DataCamp/Python/Intro_Regressions/intro.py
+++ DataCamp/Python/Intro_Regressions/intro.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 from statsmodels.formula.api import ols
 from pathlib import Path
 
@@ -20,3 +22,34 @@ lm_model = lm_model.fit()
 # check coeffs ~ params
 lm_model.params
 
+# EDA
+sns.displot(
+    x = 'price_twd_msq',
+    data = taiwan_real_estate,
+    col = 'house_age_years',
+    bins = 10
+)
+plt.show()
+
+# Categorical X-vars --> 1 of values removed by default (1st occurring ?)
+fish = pd.read_csv(
+    data_folder.joinpath('fish.csv')
+)
+
+lm_fish = ols(
+    formula = "mass_g ~ species",
+    data = fish
+).fit()
+
+lm_fish.params
+
+lm_fish2 = ols(
+    'mass_g ~ species + 0',
+    data = fish
+).fit()
+
+lm_fish2.params
+
+
+# import seaborn as sns
+# sns.histplot
