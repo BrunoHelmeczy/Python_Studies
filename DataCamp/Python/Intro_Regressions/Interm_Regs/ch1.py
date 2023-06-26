9b3353ece51fb55ac80d152e5ab2e8b70d4b20ac
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-06-26 08:24:49 +0200

    add 1st notes

--- DataCamp/Python/Intro_Regressions/Interm_Regs/ch1.py
+++ DataCamp/Python/Intro_Regressions/Interm_Regs/ch1.py
@@ -6,7 +6,42 @@ from pathlib import Path
 
 data_folder = Path().resolve().joinpath('DataCamp/Python/Intro_Regressions/data')
 
-taiwan_real_estate = pd.read_csv(
-    data_folder.joinpath('taiwan_real_estate2.csv')
+fish = pd.read_csv(
+    data_folder.joinpath('fish.csv')
 )
 
+# 1) simple lin regs
+
+## Y ~ X (numeric)
+lm_fish = ols(
+    formula = "mass_g ~ length_cm",
+    data = fish
+).fit()
+
+lm_fish.params
+
+## Y ~ X (categorical)
+lm_fish1 = ols(
+    formula = 'mass_g ~ species',
+    data = fish
+).fit()
+
+lm_fish1.params
+
+## Y ~ X (categorical - without intercept)
+lm_fish_1a = ols(
+    formula = 'mass_g ~ species + 0',
+    data = fish
+).fit()
+
+lm_fish_1a.params
+
+# 2) extend 2 MLR
+lm_fish_mlr = ols(
+    formula = 'mass_g ~ length_cm + species + 0',
+    data = fish
+).fit()
+
+lm_fish_mlr.params
+
+lm_fish_mlr.summary()
