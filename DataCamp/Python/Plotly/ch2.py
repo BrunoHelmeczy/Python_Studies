9d692c883e6184addc9186e36262afa8c906f142
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-06-29 14:29:23 +0200

    init setup for ch2 plotly

---
+++ DataCamp/Python/Plotly/ch2.py
@@ -0,0 +1,11 @@
+# %%
+import os
+
+if os.getcwd().split('\\')[-1] == 'Python_Studies':
+    from DataCamp.Python.Plotly.my_utils import *
+else:
+    from my_utils import *
+import plotly.express as px
+print(getData)
+
+# %%
