9f7e3cae0c03382ea5ec8de049e55f90724d1ddb
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-01 12:21:04 +0200

    WIP add ch3

--- DataCamp/Python/Plotly/ch3.py
+++ DataCamp/Python/Plotly/ch3.py
@@ -11,6 +11,89 @@ import plotly.graph_objects as go
 
 print(getData)
 
+# %% [markdown]
+'''
+subplots: 
+    plotly.express (px) --> add_trace()
+        * complex code
+        * not best practice (?)
+    plotly.graph_objects (go)
+'''
+
+
 # %%
+# create 1x2 subplot (hist + box)
+rev = getData('revenue_data2.csv')
+
+rev.loc[rev['Revenue'] == 'Unknown', 'Revenue'] = None
+rev = rev.dropna(subset = 'Revenue')
+
+rev['Revenue'] = rev['Revenue'].astype('int')
+# rev.astype({'Revenue': 'int'})
+
+from plotly.subplots import make_subplots
+fig = make_subplots(
+    rows = 2, cols = 1,
+    subplot_titles = [
+        'Histogram of company revenues',
+        'Box plot of company revenues'
+    ],
+    shared_xaxes = True
+)
+
+fig.add_trace(
+    go.Histogram(
+        x = rev['Revenue'], 
+        nbinsx = 5
+    ),
+    row = 1, col = 1
+).add_trace(
+    go.Box(
+        x = rev['Revenue'],
+        hovertext = rev['Company']
+    ),
+    row = 2, col = 1
+).update_layout(
+    showlegend = False,
+    title = {
+        'text': 'Plots of company revenues',
+        'x': 0.5, 'y': 0.9
+    }
+)
+
+# %%
+
 pens = getData('penguins.csv')
-pens.columns
+pens['Species'] = pens['Species'].replace(' .+', '', regex = True)
+
+fig = make_subplots(
+    rows = 3, cols = 1,
+    shared_xaxes = True
+)
+row_nr = 1
+
+# pens.columns
+
+for species in pens['Species'].unique():
+    df = pens[pens['Species'] == species]
+
+    fig.add_trace(
+        go.Scatter(
+            x = df['Culmen Length (mm)'],
+            y = df['Culmen Depth (mm)'],
+            name = species,
+            mode = 'markers'
+        ),
+        row = row_nr, col = 1
+    )
+    row_nr += 1
+
+fig.update_layout(
+    hovermode = 'x',
+    legend = {
+        'orientation': 'h',
+        'xref': 'paper', 'xanchor': 'center', 'x': 0.5
+    }
+)
+
+# %%
