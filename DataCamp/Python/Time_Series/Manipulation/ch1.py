d4579f2fecb1ee3826b952008becf50fd15ba05e
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-04 10:03:41 +0200

    add c1h data imports for time series course

---
+++ DataCamp/Python/Time_Series/Manipulation/ch1.py
@@ -0,0 +1,7 @@
+import sys # sue for importing my_utils (á la other python modules)
+sys.path.append('../Python_Studies/DataCamp/Python/Time_Series')
+from my_utils import *
+
+checkFx()
+ck = getData('nyc.csv')
+
