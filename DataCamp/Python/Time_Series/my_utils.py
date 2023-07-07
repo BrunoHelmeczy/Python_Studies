a959a17988efdcdd26443bf2938051efd12d0661
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-07 11:57:19 +0200

    add filepath fx to my_utils.py

--- DataCamp/Python/Time_Series/my_utils.py
+++ DataCamp/Python/Time_Series/my_utils.py
@@ -4,6 +4,13 @@ import pandas as pd
 def checkFx():
     print('hello there')
 
+# filename_string = 'google.csv'
+def getFilePaths(filename_string, course_folder = 'Time_Series'):
+    repo_home = Path().resolve().cwd()
+    python_path = repo_home.joinpath('DataCamp/Python')
+
+    return list(python_path.joinpath(course_folder).rglob(f'*/{filename_string}'))
+
 def getData(filename_string, course_folder = 'Time_Series'):
     repo_home = Path().resolve().cwd()
     python_path = repo_home.joinpath('DataCamp/Python')
