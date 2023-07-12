7094468767fc1b297336a80ce3e2ab335ea2c1b9
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-12 14:39:41 +0200

    add ch4 - ts manip - until matching data

--- DataCamp/Python/Time_Series/Manipulation/ch4.py
+++ DataCamp/Python/Time_Series/Manipulation/ch4.py
@@ -5,4 +5,29 @@ from my_utils import *
 
 checkFx()
 
-# 4.1) 
+# 4.1) select index comps + import data
+nyse = pd.read_excel(
+    getFilePaths('listings')[0],
+    sheet_name = 'nyse',
+    na_values = 'n/a'
+)
+
+nyse.info()
+nyse.set_index('Stock Symbol', inplace = True)
+nyse.dropna(subset = 'Sector', inplace = True)
+nyse['Market Capitalization'] = nyse['Market Capitalization'] / 1e6
+
+# select companies with largest market cap / sector
+comps = nyse.groupby('Sector')['Market Capitalization'].nlargest(1)
+comps.sort_values(ascending = False)
+
+tickers = nyse.groupby('Sector')['Market Capitalization'] \
+    .nlargest(1).index.get_level_values('Stock Symbol')
+
+pd.options.display.float_format = '{:,.2f}'.format # set print format option for float Nrs
+nyse.loc[tickers, :]
+
+# pd.read_csv(
+#     getFilePaths('stocks_4')[0]
+# )
+
