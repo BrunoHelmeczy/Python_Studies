c7123310844025214802179ba0dc8302813721cd
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-12 16:37:19 +0200

    move notes 2 ch1 - unit_testing

--- DataCamp/Python/unit_testing/tests/test_row_to_list.py
+++ DataCamp/Python/unit_testing/tests/test_row_to_list.py
@@ -2,7 +2,6 @@ import sys # use for importing my_utils (á la other python modules)
 sys.path.append('../Python_Studies/DataCamp/Python/unit_testing')
 from src.data.preprocessing_helpers import *
 
-
 # running a test: rows_to_list()
 # 1) test_row_to_list.py
     # 'test_' indicates unit tests inside (naming convention)
@@ -20,27 +19,3 @@ def test_for_missing_area():
 def test_for_missing_tab():
     assert row_to_list("1,463258,765\n") is None
 
-# 4) copy the above to seperate file: ./tests/test_row_to_list.py
-# 5) in CLI run: py -m pytest <path/to/your/test/file/test_row_to_list.py>
-    # in iPython: !pytest <path/to/your/test/file/test_row_to_list.py>
-
-# 1.3) test result reports
-    # Section 1: general info: OS; rootdir; inifile; plugins;
-    # Section 2: test results: 
-        # collected ### items';
-        # <path/to/test/file/filename.py> .F.     [100%]
-            # .F. = Pass; Fail; Pass
-            # Fails: raises AssertionError; can be other, e.g. NameError when none (instead of None)
-    # Section 3: Info on FAILED tests
-        # line with error : starts with '>'
-        # calculation with error: starts with 'E'
-        # line with 'where' in it shows returned values
-    # Section 4: test result summary
-        # Nr failed, Nr passed
-        # tests run time: 0.08s
-
-# 1.4) other test types
-
-
-
- 
