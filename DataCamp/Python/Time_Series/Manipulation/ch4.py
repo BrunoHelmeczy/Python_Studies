d1e97856cf7c649899985909e9f08e8b9cd4ccca
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-12 11:21:40 +0200

    add ch4 - ts manipulation

---
+++ DataCamp/Python/Time_Series/Manipulation/ch4.py
@@ -0,0 +1,8 @@
+import sys # sue for importing my_utils (á la other python modules)
+import matplotlib.pyplot as plt
+sys.path.append('../Python_Studies/DataCamp/Python/Time_Series')
+from my_utils import *
+
+checkFx()
+
+# 4.1) 
