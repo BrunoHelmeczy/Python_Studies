61a3dc443b4af3c0c867d9921f488a963d29e98c
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-12 16:47:33 +0200

    remove comments - ch1 unit_testing

--- DataCamp/Python/unit_testing/tests/test_row_to_list.py
+++ DataCamp/Python/unit_testing/tests/test_row_to_list.py
@@ -1,15 +1,9 @@
 import sys # use for importing my_utils (á la other python modules)
+import pytest
+
 sys.path.append('../Python_Studies/DataCamp/Python/unit_testing')
 from src.data.preprocessing_helpers import *
 
-# running a test: rows_to_list()
-# 1) test_row_to_list.py
-    # 'test_' indicates unit tests inside (naming convention)
-
-# 2) import pytest module
-import pytest
-
-# 3) define tests
 def test_for_clean_row():
     assert row_to_list("2,081\t314,942\n") == ['2,081', '314,942']
 
@@ -18,4 +12,3 @@ def test_for_missing_area():
 
 def test_for_missing_tab():
     assert row_to_list("1,463258,765\n") is None
-
