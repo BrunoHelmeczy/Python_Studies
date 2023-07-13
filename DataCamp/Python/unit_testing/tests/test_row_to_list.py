0ef0285ee32ca10b4e461005ab7689570b605737
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-13 09:36:03 +0200

    decomp test internals 2 act, exp, msg

--- DataCamp/Python/unit_testing/tests/test_row_to_list.py
+++ DataCamp/Python/unit_testing/tests/test_row_to_list.py
@@ -4,11 +4,30 @@ import pytest
 sys.path.append('../Python_Studies/DataCamp/Python/unit_testing')
 from src.data.preprocessing_helpers import *
 
+# !!! not ideal to write expression 2x
+# consider: expr = f''; actual = exec(expr) 
+    # will not work with new line characters; probably most strings
+
 def test_for_clean_row():
-    assert row_to_list("2,081\t314,942\n") == ['2,081', '314,942']
+    expr = f'row_to_list("2,081\t314,942\n")'
+    actual = row_to_list("2,081\t314,942\n")
+
+    expected = ['2,081', '314,942']
+    msg = f'{expr} is valid but did not return a list of length 2'
+    assert actual == expected, msg
 
 def test_for_missing_area():
-    assert row_to_list("\t293,410\n") is None
+    expr = f'row_to_list("\t293,410\n")'
+    actual = row_to_list("\t293,410\n")
+
+    expected = None
+    msg = f"{expr} did not return None"
+    assert actual is expected, msg
 
 def test_for_missing_tab():
-    assert row_to_list("1,463258,765\n") is None
+    expr = f'row_to_list("1,463258,765\n")'
+    actual = row_to_list("1,463258,765\n")
+
+    expected = None
+    msg = f"{expr} did not return None"
+    assert actual is expected, msg
