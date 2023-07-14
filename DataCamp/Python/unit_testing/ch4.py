47dc8eae81d467dff9c87bdd3d8108710f46f9d2
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-14 19:05:40 +0200

    add ch4.py - unit_testing

---
+++ DataCamp/Python/unit_testing/ch4.py
@@ -0,0 +1,8 @@
+# 4) Testing Models; Plots, etc.
+import numpy as np
+import pytest
+import sys
+sys.path.append('DataCamp/Python/unit_testing')
+from src.data.preprocessing_helpers import *
+from src.train.train import *
+
