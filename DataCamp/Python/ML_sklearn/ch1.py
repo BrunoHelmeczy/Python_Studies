47c84e1247412d4f2cd6122f3ee77a0b6a982755
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-07-30 11:07:30 +0200

    add knn dummy exercises

--- DataCamp/Python/ML_sklearn/ch1.py
+++ DataCamp/Python/ML_sklearn/ch1.py
@@ -6,7 +6,7 @@ from my_utils import *
 
 dfs = get_all_data_dfs(REL_PATH)
 
-# 1.2) Classification Chall
+# 1.2) Classification Challenge
 churn_df = dfs['telecom_churn_clean'].set_index(
     'Unnamed: 0', 
     drop = True
@@ -34,3 +34,22 @@ X_new = np.array([
 
 y_pred = knn.predict(X_new)
 
+# 1.2) Exercises
+
+# from sklearn.neighbors import KNeighborsClassifier
+X = churn_df[['account_length', 'customer_service_calls']].values
+y = churn_df['churn'].values
+
+knn = KNeighborsClassifier(n_neighbors = 6)
+
+knn.fit(X = X, y = y)
+
+X_new = np.array([
+    [30.0, 17.5],
+    [107.0, 24.1],
+    [213.0, 10.9]
+])
+
+y_pred = knn.predict(X_new)
+
+# 1.3) Measure Model Performance
