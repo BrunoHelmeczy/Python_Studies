319d61538fbb3e760de0cf5ce7994d8d4505eb17
Bruno Helmeczy <helmeczybruno32@gmail.com>
2023-08-06 12:57:21 +0200

    add ROC curve plotting to ch3

--- DataCamp/Python/ML_sklearn/ch3.py
+++ DataCamp/Python/ML_sklearn/ch3.py
@@ -29,7 +29,7 @@ y = churn_df['churn'].values
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, 
-    test_size = 0.1,
+    test_size = 0.3,
     random_state = 42, 
     stratify = y
 )
@@ -42,4 +42,54 @@ print(confusion_matrix(y_test, y_pred))
 print(classification_report(y_test, y_pred))
 # %%
 # 3.2) ROC curve
+from sklearn.linear_model import LogisticRegression
 
+logreg = LogisticRegression().fit(X_train, y_train)
+
+
+# %%
+y_pred = logreg.predict(X_test)
+y_pred_probs = logreg.predict_proba(X_test)[:, 1]
+
+# %%
+# vis ROC curve
+from sklearn.metrics import roc_curve, roc_auc_score
+import plotly.express as px
+
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_probs)
+auc = roc_auc_score(y_test, y_pred_probs)
+
+df = pd.DataFrame({
+    'fpr':fpr,
+    'ref_line': tpr,
+    "tpr":tpr
+}).melt(
+    id_vars = ['tpr'],
+    var_name = 'metric'
+)
+
+px.line(
+    data_frame = df,
+    x = 'value',
+    y = 'tpr',
+    color = 'metric'
+).update_layout(
+    xaxis = {'title': 'False Positive Rate', 'range': [0, 1]},
+    yaxis = {'title': 'True Positive Rate', 'range': [0, 1]}, 
+    hovermode = 'x'
+    , annotations = [{
+        'xref': 'paper',
+        'yref' : 'paper',
+        'x': 0.5,
+        'y': 0.5,
+        'xanchor': 'right',
+        'yanchor': 'bottom',
+        'text': f'AUC: <b>{auc.round(2)}</b>',
+        'showarrow': False,
+        'font': {
+            'size': 16
+        }
+    }]
+)
+
+# %%
